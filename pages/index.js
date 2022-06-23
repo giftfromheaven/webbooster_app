8b7378ea4c0cfb8626b20777fe3cad0c9cb65739
Ilya <72022227+giftfromheaven@users.noreply.github.com>
2022-06-23 13:12:33 +0300

    added modal

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 const addButtons = document.querySelectorAll('.button');
 
 function handleButton() {
-  alert('Button pushed');
+  modal.style.display = 'block';
 }
 
 for (let i = 0; i < addButtons.length; i++) {
   addButtons[i].addEventListener('click', handleButton);
 }
+
+const modal = document.getElementById('modal');
+
+const span = document.getElementsByClassName('modal__close')[0];
+
+addButtons.onclick = function () {
+  modal.style.display = 'block';
+};
+
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
